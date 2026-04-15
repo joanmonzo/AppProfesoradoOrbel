74e10b2c7072e6641a9bd8906acd45674ceb601b
joanmonzo <joan.monzo@orbelgrupo.com>
2026-04-15 12:22:08 +0200

    Arreglo firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
+// Las credenciales ahora se leen de forma segura desde las variables de entorno
 const firebaseConfig = {
-  apiKey: "AIzaSyBQ0RXZMaRIeDLfA7TJNi9YiAeYFl1YEuY",
-  authDomain: "appprofesorado.firebaseapp.com",
-  projectId: "appprofesorado",
-  storageBucket: "appprofesorado.firebasestorage.app",
-  messagingSenderId: "579306310987",
-  appId: "1:579306310987:web:bacd2e54828bd6c5e0ecce",
-  measurementId: "G-BSP556P3J7",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const analytics = getAnalytics(app);
